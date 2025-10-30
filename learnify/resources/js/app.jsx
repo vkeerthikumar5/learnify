4343644aea28dfbd514da0c60d3b93ff02f0300f
vkeerthikumar5 <146057811+vkeerthikumar5@users.noreply.github.com>
2025-10-30 12:27:58 +0530

    Delete learnify directory

--- learnify/resources/js/app.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './pages/App';
-import '../css/app.css'
-import "@fortawesome/fontawesome-free/css/all.min.css";
-
-const root = createRoot(document.getElementById('app'));
-root.render(<App />);
--- learnify/routes/web.php
+++
@@ -1,6 +0,0 @@
-<?php
-use Illuminate\Support\Facades\Route;
-
-Route::get('/{any}', function () {
-    return view('welcome'); 
-})->where('any', '^(?!api).*$'); // ignore any route starting with "api"
