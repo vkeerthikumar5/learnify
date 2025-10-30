8707cffd2ebab7fc5cdc168beab8458aac491205
Keerthi Kumar <vkeerthikumar5@gmail.com>
2025-10-30 21:08:38 +0530

    Changes

---
+++ backend/config/cors.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'https://learnify-phi-ten.vercel.app/', // your frontend domain
+        'http://localhost:5173', // optional, for local testing
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
+
+];
