fb40ffafa7f2b04b061db310ff6ea04faed04b5f
Keerthi Kumar <vkeerthikumar5@gmail.com>
2025-11-01 16:56:04 +0530

    Backend Changes

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -2,14 +2,24 @@
 
 return [
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your settings for cross-origin resource sharing
+    | or "CORS". This determines what cross-origin operations may execute
+    | in web browsers. You are free to adjust these settings as needed.
+    |
+    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+    |
+    */
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie','*'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'https://learnify-phi-ten.vercel.app/', // your frontend domain
-        'http://localhost:5173', // optional, for local testing
-    ],
+    'allowed_origins' => [env('FRONTEND_URL')],
 
     'allowed_origins_patterns' => [],
 
@@ -19,6 +29,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 
 ];
