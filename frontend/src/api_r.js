65ef7cb5c692548d93422a79fd481de484c69ee4
Keerthi Kumar <vkeerthikumar5@gmail.com>
2025-11-01 16:15:59 +0530

    Backend Changes

--- frontend/src/api_r.js
+++ frontend/src/api_r.js
@@ -1,20 +1,14 @@
-import axios from "axios";
+import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "https://learnify.rf.gd/public/proxy.php?endpoint=",
+  baseURL: 'https://learnify.rf.gd/public/api/',
 });
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem('token');
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
-
-  // Remove leading slash if exists
-  if (config.url.startsWith("/")) {
-    config.url = config.url.substring(1);
-  }
-
   return config;
 });
 
