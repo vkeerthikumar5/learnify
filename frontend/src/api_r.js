1e40a20d916951eef998c030d6ed4baa4722378a
Keerthi Kumar <vkeerthikumar5@gmail.com>
2025-11-01 15:57:04 +0530

    API Changes

--- frontend/src/api_r.js
+++ frontend/src/api_r.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://learnify.rf.gd/public/proxy.php?endpoint=",
+  paramsSerializer: params => new URLSearchParams(params).toString(),
 });
 
 api.interceptors.request.use((config) => {
@@ -9,6 +10,12 @@ api.interceptors.request.use((config) => {
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
+  // Remove starting slash from the request URL if it exists
+  if (config.url.startsWith("/")) {
+    config.url = config.url.substring(1);
+  }
+
   return config;
 });
 
