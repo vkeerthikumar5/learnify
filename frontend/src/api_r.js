9fc2ea3c823fb8f0cb1db34c3e0dc1b52ff3425b
Keerthi Kumar <vkeerthikumar5@gmail.com>
2025-11-03 17:19:42 +0530

    Update api

--- frontend/src/api_r.js
+++ frontend/src/api_r.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://learnify.rf.gd/public/api',
+  baseURL: 'https://learnify.rf.gd/api',
 });
 
 api.interceptors.request.use((config) => {
